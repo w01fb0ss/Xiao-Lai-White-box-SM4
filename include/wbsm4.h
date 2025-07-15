ee32a13e7c03805ce8e2e16580d2dcd03589c2e8
w01fb0ss <leen.zhou.cn@gmail.com>
2025-07-15 15:00:07 +0800

    add wbsm4

--- include/wbsm4.h
+++ include/wbsm4.h
@@ -20,6 +20,16 @@ extern Aff32 SE[4];
 extern Aff32 FE[4];
 extern uint32_t Table[32][4][256];
 
+typedef struct {
+    Aff32 M[32][3];              // 32轮，每轮3个 Aff32
+    Aff32 C[32];                 // 32轮常量 C
+    Aff32 D[32];                 // 32轮常量 D
+    Aff32 SE[4];                 // 输入部分的 Aff32 变换
+    Aff32 FE[4];                 // 输出部分的 Aff32 变换
+    uint32_t Table[32][4][256];  // 32轮，每轮4个字节，每个字节256个值
+} WhiteBoxLut;
+
+
 void printstate(unsigned char * in);
 void wbsm4_gen(uint8_t *key);
 void wbsm4_encrypt(unsigned char IN[], unsigned char OUT[]);
